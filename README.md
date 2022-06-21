0c2706fa71bf2c45f270f0830dca3408946fb2fa
ArturCodes <arturshapiro@gmail.com>
2022-06-21 05:58:34 -0500

    .

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # artsha
  Artsha Web Design and Development
+
+
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_push_opacity
--- mobileNav.js
+++ mobileNav.js
@@ -1,3 +1,13 @@
-const mobileNavOuter = document.getElementsByClassName('mobile-menu-outer"')
-const mobileNavInner = document.getElementsByClassName('mobile-menu-inner"')
+// Outer hamburger butten to open mobile menu
+const mobileNavOuter = document.getElementsByClassName('mobile-menu-outer')
 
+// Inner X Button to close mobile menu
+const mobileNavInner = document.getElementsByClassName('mobile-menu-inner')
+
+const body = document.body;
+ 
+closeMobileMenu = () => {
+    if (mobileNavInner) {
+        mobileNavInner.classList.replace('open', 'close')
+    }
+}
