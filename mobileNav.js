3e63a92d78b23ee0ea0de677d7b26b49bcc1c9f1
Artur Shapiro <arturshapiro@gmail.com>
2022-06-28 03:25:50 -0500

    Theme's Next

--- mobileNav.js
+++ mobileNav.js
@@ -1,8 +1,10 @@
 
 function openNav() {
     document.getElementById('mobile-nav').classList.replace('closeNav', 'openNav');
+    document.getElementById('opacity-bg').classList.replace('closeNav', 'openNav');
 }
 
 function closeNav() {
     document.getElementById('mobile-nav').classList.replace('openNav', 'closeNav');
+    document.getElementById('opacity-bg').classList.replace('openNav', 'closeNav');
 }
