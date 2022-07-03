987639aeb1f7fe21cc0d54af968240ef58e16f90
ArturCodes <arturshapiro@gmail.com>
2022-07-03 17:29:56 -0500

    .

--- themeChanger.js
+++ themeChanger.js
@@ -12,17 +12,25 @@ if (theme) {
  
 function themeChange() {
     const body = document.body;
+    const img = document.getElementById('logo')
+    const theme = document.getElementById('theme')
     
     if (body.classList.contains('light')) {
         body.classList.replace('light', 'dark');
+        img.src="./assets/logo-white.png";
+        theme.src="./assets/theme-white.png"
         localStorage.setItem('theme', 'dark');
 
     } else if (body.classList.contains('dark')) {
         body.classList.replace('dark', 'green');
+        img.src="./assets/logo-white.png";
+        theme.src="./assets/theme-white.png"
         localStorage.setItem('theme', 'green');
 
     } else if (body.classList.contains('green')) {
         body.classList.replace('green', 'light');
+        img.src="./assets/logo.png";
+        theme.src="./assets/theme.png"
         localStorage.setItem('theme', 'light');
     }
 }
