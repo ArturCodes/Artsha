612dab60b8adc918248b8e15585aef86101fec52
ArturCodes <arturshapiro@gmail.com>
2022-07-04 01:41:22 -0500

    Progress... finally

--- themeChanger.js
+++ themeChanger.js
@@ -1,36 +1,57 @@
 
 
 // Check for theme & set it
-if (theme) {
-    const body = document.body;
-    const theme = localStorage.getItem('theme')
+// if (theme) {
+//     const body = document.body;
+//     const theme = localStorage.getItem('theme')
 
-    body.removeAttribute('class');
-    body.classList.add(theme)
-}
+//     body.removeAttribute('class');
+//     body.classList.add(theme)
+// }
 
- 
-function themeChange() {
+
+function checkTheme() {
     const body = document.body;
     const img = document.getElementById('logo')
     const theme = document.getElementById('theme')
     
+    if (body.classList.contains('dark')) {
+        theme.src = "./assets/theme-white.png";
+        img.src = "./assets/logo-white.png";
+
+    } else if (body.classList.contains('light')) {
+        img.src = "./assets/logo.png";
+        theme.src = "./assets/theme.png"
+        
+    } else if (body.classList.contains('green')) {
+        theme.src = "./assets/theme-white.png";
+        img.src = "./assets/logo-white.png";
+    }
+
+}
+
+
+function themeChange() {
+    const body = document.body;
+    const img = document.getElementById('logo')
+    const theme = document.getElementById('theme')
+
     if (body.classList.contains('light')) {
         body.classList.replace('light', 'dark');
-        img.src="./assets/logo-white.png";
-        theme.src="./assets/theme-white.png"
+        img.src = "./assets/logo-white.png";
+        theme.src = "./assets/theme-white.png"
         localStorage.setItem('theme', 'dark');
 
     } else if (body.classList.contains('dark')) {
         body.classList.replace('dark', 'green');
-        img.src="./assets/logo-white.png";
-        theme.src="./assets/theme-white.png"
+        img.src = "./assets/logo-white.png";
+        theme.src = "./assets/theme-white.png"
         localStorage.setItem('theme', 'green');
 
     } else if (body.classList.contains('green')) {
         body.classList.replace('green', 'light');
-        img.src="./assets/logo.png";
-        theme.src="./assets/theme.png"
+        img.src = "./assets/logo.png";
+        theme.src = "./assets/theme.png"
         localStorage.setItem('theme', 'light');
     }
 }
