5affc966cc26cf299cef6d6e31c592491bd2e79b
Artur Shapiro <arturshapiro@gmail.com>
2022-07-14 07:02:56 -0500

    .

--- themeChanger.js
+++ themeChanger.js
@@ -1,49 +1,52 @@
 
 
 
-function checkTheme() {
+// function checkTheme() {
+//     const body = document.body;
+//     const img = document.getElementById('logo')
+//     const theme = document.getElementById('theme')
+
+//     if (body.classList.contains('dark')) {
+//         theme.src = "./assets/theme-white.png";
+//         img.src = "./assets/logo-white.png";
+
+//     } else if (body.classList.contains('light')) {
+//         img.src = "./assets/logo.png";
+//         theme.src = "./assets/theme.png"
+//     }
+
+
+// }
+
+function changeTheme() {
     const body = document.body;
-    const img = document.getElementById('logo')
-    const theme = document.getElementById('theme')
-
-    if (body.classList.contains('dark')) {
-        theme.src = "./assets/theme-white.png";
-        img.src = "./assets/logo-white.png";
-
-    } else if (body.classList.contains('blue')) {
-        theme.src = "./assets/theme-white.png";
-        img.src = "./assets/logo-white.png";
-
-    } else if (body.classList.contains('light')) {
-        img.src = "./assets/logo.png";
-        theme.src = "./assets/theme.png"
-    }
-        
-
-}
-
-
-function themeChange() {
-    const body = document.body;
-    const img = document.getElementById('logo')
-    const theme = document.getElementById('theme')
+    const theme = document.getElementById('body');
+    const img = document.getElementById('logo');
 
     if (body.classList.contains('light')) {
         body.classList.replace('light', 'dark');
-        img.src = "./assets/logo-white.png";
-        theme.src = "./assets/theme-white.png"
-        localStorage.setItem('theme', 'dark');
+        img.src = "./assets/logo.png";
+        theme.src = "./assets/theme.png";
 
     } else if (body.classList.contains('dark')) {
-        body.classList.replace('dark', 'blue');
+        body.classList.replace('dark', 'light');
+        theme.src = "./assets/theme-white.png";
         img.src = "./assets/logo-white.png";
-        theme.src = "./assets/theme-white.png"
-        localStorage.setItem('theme', 'blue');
-
-    } else if (body.classList.contains('blue')) {
-        body.classList.replace('blue', 'light');
-        img.src = "./assets/logo.png";
-        theme.src = "./assets/theme.png"
-        localStorage.setItem('theme', 'light');
     }
 }
+// function themeChange() {
+//     const body = document.body;
+//     const img = document.getElementById('logo')
+//     const theme = document.getElementById('theme')
+//     if (body.classList.contains('light')) {
+//         body.classList.replace('light', 'dark');
+//         img.src = "./assets/logo-white.png";
+//         theme.src = "./assets/theme-white.png";
+//         localStorage.setItem('theme', 'dark');
+//     } else (body.classList.contains('dark')) {
+//         body.classList.replace('dark', 'light');
+//         img.src = "./assets/logo-white.png";
+//         theme.src = "./assets/theme-white.png";
+//         localStorage.setItem('theme', 'blue');
+//      }
+// }
