680cfdde91a21d2876a6a2664a390fa3b3e69063
ArturCodes <arturshapiro@gmail.com>
2022-07-15 03:47:04 -0500

    .

--- themeChanger.js
+++ themeChanger.js
@@ -1,35 +1,34 @@
 
 
-
-// function checkTheme() {
-//     const body = document.body;
-//     const img = document.getElementById('logo')
-//     const theme = document.getElementById('theme')
-
-//     if (body.classList.contains('dark')) {
-//         theme.src = "./assets/theme-white.png";
-//         img.src = "./assets/logo-white.png";
-
-//     } else if (body.classList.contains('light')) {
-//         img.src = "./assets/logo.png";
-//         theme.src = "./assets/theme.png"
-//     }
-
-
-// }
-
-function changeTheme() {
+function changeTheme()  {
     const body = document.body;
-    const theme = document.getElementById('body');
+    const img = document.getElementById('logo')
+    const theme = document.getElementById('theme')
+
+    if (body.classList.contains('dark')) {
+        body.classList.replace('light', 'dark');
+        theme.src = "./assets/theme-white.png";
+        img.src = "./assets/logo-white.png";
+
+    } else if (body.classList.contains('light')) {
+        body.classList.replace('dark', 'light');
+        img.src = "./assets/logo.png";
+        theme.src = "./assets/theme.png"
+    }
+
+
+}
+
+function checkTheme()  {
+    const body = document.body;
+    const theme = document.getElementById('theme');
     const img = document.getElementById('logo');
 
     if (body.classList.contains('light')) {
-        body.classList.replace('light', 'dark');
         img.src = "./assets/logo.png";
         theme.src = "./assets/theme.png";
 
     } else if (body.classList.contains('dark')) {
-        body.classList.replace('dark', 'light');
         theme.src = "./assets/theme-white.png";
         img.src = "./assets/logo-white.png";
     }
